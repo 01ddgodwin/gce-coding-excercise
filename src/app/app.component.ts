64ede857c4d66693f7098160a4ba0b01568384de
Dillon Godwin <ddgodwin01@byui.edu>
2022-09-29 16:10:38 -0600

    Updated year validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,13 @@ export class AppComponent {
 
     // Check to see if selects and input are stored in sessionStorage and
     // if its not then fill with current date
-    if (this.selectedMonth === null) {
+    if (this.selectedMonth === null || this.selectedDay === null || this.yearInput === null) {
       this.selectedMonth = String(this.currentMonthNumber);
-    } else {
-      this.selectedMonth = sessionStorage.getItem('selectedMonth');
-    }
-
-    if (this.selectedDay === null) {
       this.selectedDay = String(this.currentDay);
-    } else {
-      this.selectedDay = sessionStorage.getItem('selectedDay');
-    }
-
-    if (this.yearInput === null) {
       this.yearInput = String(this.currentYear);
     } else {
+      this.selectedMonth = sessionStorage.getItem('selectedMonth');
+      this.selectedDay = sessionStorage.getItem('selectedDay');
       this.yearInput = sessionStorage.getItem('yearInput');
     }
 
@@ -55,14 +47,12 @@ export class AppComponent {
   //Combined all selected inputs into one date
   selectedDate = this.selectedMonth + '/' + this.selectedDay + '/' + this.yearInput;
 
-  newSelectedDate: string = this.selectedDate;
-  date: Date = new Date(this.selectedDate);
-  dateDifference: number = (this.date.getTime() - this.currentDate.getTime()) / (1000 * 3600 * 24) + 1;
+  date: Date = new Date(this.selectedDate);  dateDifference: number = (this.date.getTime() - this.currentDate.getTime()) / (1000 * 3600 * 24) + 1;
 
   // When the inputs change, this function will run updating up the variables
   updateDate() {
-    this.newSelectedDate = this.selectedMonth + '/' + this.selectedDay + '/' + this.yearInput;
-    this.date = new Date(this.newSelectedDate);
+    this.selectedDate = this.selectedMonth + '/' + this.selectedDay + '/' + this.yearInput;
+    this.date = new Date(this.selectedDate);
 
     // Get the difference between two dates
     this.dateDifference = (this.date.getTime() - this.currentDate.getTime()) / (1000 * 3600 * 24) + 1;
